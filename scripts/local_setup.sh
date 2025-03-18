5fef943d7b6cc82d11823dda7b8a0683a52bf655
leozhao0709 <leozhao0709@users.noreply.github.com>
2025-03-18 10:02:57 -0700

    minor update (#3)

--- scripts/local_setup.sh
+++ scripts/local_setup.sh
@@ -32,7 +32,7 @@ check_and_install "protoc-gen-go-grpc" "go install google.golang.org/grpc/cmd/pr
 check_and_install "wire" "go install github.com/google/wire/cmd/wire@latest"
 check_and_install "mockery" "go install github.com/vektra/mockery/v2@v2.46.0"
 check_and_install "goose" "go install github.com/pressly/goose/v3/cmd/goose@latest"
-check_and_install "openapi-generator-cli" "brew install openapi-generator"
+check_and_install "openapi-generator" "brew install openapi-generator"
 
 # setup tools
 if [ ! -d "google" ]; then
@@ -47,10 +47,18 @@ else
     echo ".envrc already exists, skipping ✅"
 fi
 
+# Check if current directory is a git repository
+if [ ! -d ".git" ]; then
+    echo "Initializing git repository ✅"
+    git init
+else
+    echo "Git repository already exists ✅"
+fi
+
 # lefthook
 lefthook install
 
 # setup project
 go mod tidy -e
 
-make check
+make generate
