611af5ea3b1506ebd99c2eb3579020f36f3f9f88
leozhao9527 <leo.zhao.real@gmail.com>
2025-06-29 15:56:50 -0700

    minor update

--- internal/infrastructure/http/openapi/v1/handler/user/get_users.go
+++ internal/infrastructure/http/openapi/v1/handler/user/get_users.go
@@ -1,12 +1,11 @@
 package user
 
 import (
-	"encoding/json"
-	"log/slog"
 	"net/http"
 
 	"github.com/umefy/go-web-app-template/internal/infrastructure/http/openapi/v1/handler/user/mapping"
 	api "github.com/umefy/go-web-app-template/openapi/generated/go/openapi"
+	"github.com/umefy/godash/jsonkit"
 	"github.com/umefy/godash/sliceskit"
 )
 
@@ -24,13 +23,5 @@ func (h *userHandler) GetUsers(w http.ResponseWriter, r *http.Request) error {
 		Data: sliceskit.Map(users, mapping.UserModelToApiUser),
 	}
 
-	h.loggerService.DebugContext(ctx, "resp", slog.Any("resp", resp))
-	// return jsonkit.JSONResponse(w, http.StatusOK, &resp)
-	v, err := json.Marshal(resp)
-	if err != nil {
-		return err
-	}
-
-	w.Write(v)
-	return nil
+	return jsonkit.JSONResponse(w, http.StatusOK, &resp)
 }
